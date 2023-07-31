24db18f5efe3ff2ad2182283fe9a3648fd23bd55
Mahyar Kazazi <rihem1386@gmail.com>
2023-07-31 12:56:51 +0330

    Change Project Files to BluePrint

--- Source/FPS_TTW/FPS_TTW.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FPS_TTW.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPS_TTW, "FPS_TTW" );
- 
